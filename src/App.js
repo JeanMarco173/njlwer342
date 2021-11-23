4a5e0d91c2b803dd35aa5385524eb06fe2266854
JeanMarko173 <jeanmarko.13.7@gmail.com>
2021-11-23 10:44:00 -0500

    Agrega Tareas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 /// Modifica el componente para que se puedan agregar tareas
 
-class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
-          </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
-        </div>
-      </div>
-    )
+export default function App () {
+
+  const [ tasks, setTasks ] = useState(['Sacar la ropa', 'Hacer la cama', 'Leer un rato'])
+  const [ task, setTask ] = useState('')
+
+  const handlePressEnter = e => {
+    if(e.key === 'Enter'){
+      let auxTasks = [...tasks]
+      auxTasks.push(task)
+      setTasks(auxTasks)
+      setTask('')
+    }
   }
+
+  return (
+    <div className="wrapper">
+      <div className="list">
+        <h3>Por hacer:</h3>
+        <ul className="todo">
+          {
+            tasks.map( (task, index) => <li key={index}>{task}</li> )
+          }
+        </ul>
+          <form onSubmit={e => { e.preventDefault(); }}>
+            <input type="text" id="new-task" value={task} onChange={ e => setTask(e.target.value) } onKeyDown={handlePressEnter} placeholder="Ingresa una tarea y oprime Enter" />
+          </form>
+      </div>
+    </div>
+  )
 }
-
-
-export default App;
